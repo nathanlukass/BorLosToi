7fa20ef08590d67dcb7dcb0fc03be6b751471bf7
jonathanlukass <jhonatanlukas05@gmail.com>
2024-08-21 12:46:03 +0800

    asd

---
+++ src/pages/SplashScreen/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const SplashScreen = () => {
+  return (
+    <View>
+      <Text> PILIHAN</Text>
+    </View>
+  );
+};
+
+export default SplashScreen;
---
+++ src/pages/index.tsx
@@ -0,0 +1,4 @@
+import SplashScreen from './SplashScreen';
+import Login from './Login';
+
+export {SplashScreen, Login};
