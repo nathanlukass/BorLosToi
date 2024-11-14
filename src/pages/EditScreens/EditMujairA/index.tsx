1b7d687c5514bf9d5af093c6ea5294c762d47fe7
Rillcuyyy <sangariariel@gmail.com>
2024-11-14 11:46:02 +0800

    input & edit screen

--- src/pages/EditScreens/EditMujairA/index.tsx
+++ src/pages/EditScreens/EditMujairA/index.tsx
@@ -27,7 +27,7 @@ const EditMujairA = ({route}) => {
   const {user} = route.params;
   const {ruangan} = user;
   const navigation = useNavigation<StackNavigationProp<ParamListBase>>();
-
+  const titleRuangan = "Mujair A";
   const [jumlahTempatTidur, setJumlahTempatTidur] = useState('22');
   const [selectedDate, setSelectedDate] = useState('');
   const [pasienAwal, setPasienAwal] = useState('0');
@@ -47,7 +47,7 @@ const EditMujairA = ({route}) => {
   const [kelas1, setKelas1] = useState('0');
   const [kelas2, setKelas2] = useState('0');
   const [kelas3, setKelas3] = useState('0');
-  const [namaruangan, setRuangan] = useState(ruangan);
+  const [namaruangan, setRuangan] = useState(titleRuangan);
 
   const increment = setter => () =>
     setter(prev => (parseInt(prev, 10) + 1).toString());
@@ -71,6 +71,7 @@ const EditMujairA = ({route}) => {
           },
           body: new URLSearchParams({
             date: formattedDate,
+            ruangan: titleRuangan
           }).toString(),
         },
       );
@@ -192,7 +193,7 @@ const EditMujairA = ({route}) => {
               source={require('../../../../assets/-icon-arrow-back.png')}
             />
           </Pressable>
-          <Text style={styles.headerTitle}>{'Mujair A'}</Text>
+          <Text style={styles.headerTitle}>{titleRuangan}</Text>
         </View>
 
         <View style={styles.timeInfoContainer}>
@@ -203,11 +204,13 @@ const EditMujairA = ({route}) => {
           onDateChange={handleDateChange}
         />
 
-        <View style={styles.section}>
+        {/* Fields with increment/decrement buttons */}
+        <View style={styles.sectionJumlahBed}>
           <Text style={styles.label}>Jumlah tempat tidur:</Text>
           <Text style={styles.jumlahBed}>{jumlahTempatTidur}</Text>
         </View>
 
+        {/* Existing sections */}
         <View style={styles.section}>
           <Text style={styles.sectionTitle}>Pasien Awal</Text>
           {renderInputField('Pasien awal', pasienAwal, setPasienAwal)}
@@ -222,48 +225,35 @@ const EditMujairA = ({route}) => {
             setPasienPindahan,
           )}
         </View>
-
         <View style={styles.section}>
-          <Text style={styles.sectionTitle}>Pasien Dipindahkan</Text>
+        <Text style={styles.sectionTitle}>Pasien Keluar Ruangan</Text>
+        <View style={styles.row}>
+        <Text style={[styles.subsectionTitle, { marginRight: 6 }]}>Pasien keluar</Text>
+        <Text style={[styles.subsectionTitle, { color: '#00A676' }]}>Hidup</Text>
+        </View>
           {renderInputField(
             'Pasien dipindahkan',
             pasienDipindahkan,
             setPasienDipindahkan,
           )}
-        </View>
-
-        <View style={styles.section}>
-          <Text style={styles.sectionTitle}>Pasien Keluar Ruangan</Text>
           {renderInputField('Hidup', pasienHidup, setPasienHidup)}
           {renderInputField('Rujuk', pasienRujuk, setPasienRujuk)}
           {renderInputField('APS', pasienAps, setPasienAps)}
           {renderInputField('Lain-lain', pasienLainLain, setPasienLainLain)}
+
+        {/* Pasien keluar Meninggal */}
+        <View style={styles.row}>
+        <Text style={[styles.subsectionTitle, { marginRight: 6 }]}>Pasien keluar</Text>
+        <Text style={[styles.subsectionTitle, { color: '#FF5A5F' }]}>Meninggal</Text>
+        </View>       
+          {renderInputField('Kurang dari 48 jam', pasienKurangDari48Jam, setPasienKurangDari48Jam)}
+          {renderInputField('Lebih dari 48 jam', pasienLebihDari48Jam, setPasienLebihDari48Jam)}
         </View>
 
         <View style={styles.section}>
-          <Text style={styles.sectionTitle}>Additional Patient Info</Text>
-          {renderInputField(
-            'Kurang dari 48 jam',
-            pasienKurangDari48Jam,
-            setPasienKurangDari48Jam,
-          )}
-          {renderInputField(
-            'Lebih dari 48 jam',
-            pasienLebihDari48Jam,
-            setPasienLebihDari48Jam,
-          )}
-          {renderInputField(
-            'Masih dirawat',
-            pasienMasihDirawat,
-            setPasienMasihDirawat,
-          )}
-          {renderInputField(
-            'Lama dirawat',
-            pasienLamaDirawat,
-            setPasienLamaDirawat,
-          )}
-          {renderInputField('Banyak pasien', banyakPasien, setBanyakPasien)}
-          {renderInputField('Jumlah hari', jumlahHari, setJumlahHari)}
+        <Text style={styles.sectionTitle}>Pasien yang masih dirawat</Text>
+          {renderInputField('Lama dirawat', pasienLamaDirawat, setPasienLamaDirawat)}
+          {renderInputField('Pasien keluar/masuk \npada hari yang sama', banyakPasien, setBanyakPasien)}
         </View>
 
         <View style={styles.section}>
@@ -286,47 +276,108 @@ const EditMujairA = ({route}) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F7F7F7',
+    backgroundColor: '#FFFFFFF',
   },
   scrollContainer: {
     padding: 20,
     paddingBottom: 40,
   },
+  row: {
+    top: 2,
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 12,
+  },
+  subsectionTitle: {
+    fontSize: 14,
+    fontFamily: FontFamily.poppinsBold,
+    color: Color.notSoBlack,
+    marginTop: 15,
+    marginBottom: 10,
+  },
+  totalContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginEnd: 75,
+    paddingVertical: 5,
+  },
+  totalLabel: {
+    fontSize: 14,
+    fontFamily: FontFamily.poppinsRegular,
+    color: Color.notSoBlack,
+  },
+  totalValue: {
+    fontSize: 14,
+    fontFamily: FontFamily.poppinsBold,
+    color: Color.notSoBlack,
+    textAlign: 'right',
+  },
   header: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
-    marginBottom: 20,
   },
   headerTitle: {
-    fontSize: 18,
+    fontSize: 20,
     fontFamily: FontFamily.poppinsBold,
     color: Color.notSoBlack,
-    marginStart: 'auto',
-    marginEnd: 'auto',
-    left: -10,
+    textAlign: 'center',
+    flex: 1,
   },
   timeInfoContainer: {
     backgroundColor: '#007BFF',
     paddingVertical: 10,
     borderRadius: 5,
     alignItems: 'center',
-    marginBottom: 20,
+    marginBottom: 15,
+    marginTop: 55,
   },
   timeInfoText: {
     color: '#FFFFFF',
     fontSize: 14,
     fontFamily: FontFamily.poppinsRegular,
   },
+  sectionJumlahBed : {
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    backgroundColor: '#FFFFFF',
+    borderRadius: 10,
+    padding: 15,
+    marginBottom: 20,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    elevation: 6,
+  },
+  sectionHeader: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF',
+    paddingVertical: 15,
+    paddingHorizontal: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    elevation: 6,
+    width: '120%',
+    position: 'absolute',
+    alignSelf: 'center',
+    justifyContent:'center',
+  },
+  
   section: {
     backgroundColor: '#FFFFFF',
     borderRadius: 10,
     padding: 15,
     marginBottom: 20,
-    shadowColor: 'rgba(0, 0, 0, 0.1)',
-    shadowOffset: {width: 0, height: 2},
-    shadowOpacity: 0.8,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.25,
     shadowRadius: 8,
+    elevation: 6,
   },
   fieldContainer: {
     flexDirection: 'row',
@@ -342,22 +393,30 @@ const styles = StyleSheet.create({
     shadowRadius: 8,
   },
   jumlahBed: {
-    fontSize: 15,
+    fontWeight: 'bold',
+    fontSize: 16,
     fontFamily: FontFamily.poppinsRegular,
     color: Color.notSoBlack,
+    marginLeft: 155,
   },
   sectionTitle: {
-    fontSize: 15,
-    fontWeight: 'bold',
+    fontSize: 16,
+    fontFamily: FontFamily.poppinsBold,
     color: Color.notSoBlack,
     marginBottom: 10,
     marginTop: 10,
   },
+  label: {
+    fontSize: 16,
+    fontFamily: FontFamily.poppinsRegular,
+    color: Color.notSoBlack,
+  },
   inputContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'flex-start',
+    justifyContent: 'space-between',
     marginLeft: 'auto',
+    marginBottom:5,
   },
   button: {
     width: 30,
@@ -375,30 +434,32 @@ const styles = StyleSheet.create({
   },
   buttonText: {
     color: '#FFFFFF',
-    fontSize: 18,
-    fontWeight: 'bold',
+    fontSize: 16,
+    fontFamily: FontFamily.poppinsBold,
   },
   input: {
     width: 40,
-    height: 40,
+    height: 35,
     textAlign: 'center',
     fontSize: 15,
+    fontFamily: FontFamily.poppinsRegular,
     borderWidth: 1,
     borderColor: '#CCCCCC',
     borderRadius: 5,
     marginHorizontal: 5,
-    paddingTop: 12,
+    paddingBottom: 5,
+    paddingVertical: 5,
   },
   submitButton: {
     backgroundColor: '#28A745',
     paddingVertical: 15,
-    borderRadius: 25,
+    borderRadius: 50,
     alignItems: 'center',
-    marginTop: 15,
+    marginTop: 10,
   },
   submitText: {
     color: '#FFFFFF',
-    fontSize: 16,
+    fontSize: 18,
     fontFamily: FontFamily.poppinsBold,
   },
   iconArrowBack: {
